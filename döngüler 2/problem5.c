8ba24fc7f41ec538e0f4cf81f920ae9be2319934
mertegesngr <mertegesungur5@gmail.com>
2024-10-26 19:50:32 +0300

    problem5.c

--- döngüler 2/problem5.c
+++ döngüler 2/problem5.c
@@ -1,42 +1,24 @@
 #include <stdio.h>
 
-int main () {
+int main() {
+    int sayi, gecici;
 
-int sayi , gecici ; 
+    // Kullanıcıdan bir sayı girmesini iste
+    printf("Sayi: ");
+    scanf("%d", &sayi);
+    printf("\n");
 
-printf("sayi : ") ; scanf("%d",&sayi) ; printf("\n") ; 
+    // Sayıyı geçici değişkene ata, böylece asıl değeri değiştirmeden işlemler yapılabilir
+    gecici = sayi;
 
+    // Sayının basamaklarını ters sırayla yazdır
+    while (gecici > 0) {
+        int basamak = gecici % 10;  // Sayının son basamağını al
+        printf("%d", basamak);      // Basamağı ekrana yazdır
+        gecici = gecici / 10;       // Sayıyı bir basamak sağa kaydırarak küçült
+    }
 
-gecici=sayi ;
+    printf("\n"); // Basamakları ters yazdırdıktan sonra yeni satıra geç
 
-
-while (gecici>0){
-
-int basamak = gecici%10 ; 
-
-printf("%d",basamak) ;
-gecici = gecici/10 ; 
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-return 0 ;
+    return 0;
 }
